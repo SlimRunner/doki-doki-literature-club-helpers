b1c87c4c11a4d2a5b5605c624898b56b75580907
SlimRunner <dv.polygon@gmail.com>
2025-09-08 23:25:39 -0700

    Allow mapper to be less strict for better sorting

--- utils/aliases.py
+++ utils/aliases.py
@@ -1,5 +1,5 @@
 from collections.abc import Callable
-from typing import TypeAlias
+from typing import TypeAlias, Any
 
 PtVector: TypeAlias = tuple[int, int, int]
 PtVectorFilter: TypeAlias = Callable[[int, int, int], bool]
@@ -8,4 +8,4 @@ WordPointCellRaw: TypeAlias = tuple[str, str, str, str]
 WordPointCell: TypeAlias = tuple[str, int, int, int]
 WordPoints: TypeAlias = list[WordPointCell]
 WordVectorFilter: TypeAlias = Callable[[str, int, int, int], bool]
-WordVectorMap: TypeAlias = Callable[[str, int, int, int], WordPointCell]
+WordVectorMap: TypeAlias = Callable[[Any, Any, Any, Any], WordPointCell]
